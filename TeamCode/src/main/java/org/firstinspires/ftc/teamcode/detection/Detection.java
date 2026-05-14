30df90c96fa053203a4dc5dae5427d09457be14d
Theotaran <eusunttheo@gmail.com>
2026-05-14 21:36:20 +0300

    :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/detection/Detection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/detection/Detection.java
@@ -4,7 +4,19 @@ public class Detection {
     private Camera camera;
     private Turret turret;
 
+    private double goalX, goalY, goalZ, goalXrobot, goalYrobot, goalZrobot, goalXfield, goalYfield, goalZfield;
+    private static double xoffset, yoffset;
+
     public void update(){
+        if(!camera.detected())
+            return;
+
+
+
+        goalX = camera.getGoalX();
+        goalY = camera.getGoalY();
+
+        goalXrobot = goalX * Math.sin(Math.toRadians(turret.getAngle()));
 
     }
 }
