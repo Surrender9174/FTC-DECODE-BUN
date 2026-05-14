5903b2623143ef83c3cfe16f6c106fa11187240d
JustSoulI <tibuiustin@icloud.com>
2026-05-14 14:44:36 +0300

    WIP Rewrote Spindexer, and wrote algorithm to change inbetween degrees and ticks, more correction to be followed, this is not stable.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Objects/Indexer/SpindexerOLD.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.Objects.Indexer;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.firstinspires.ftc.teamcode.robot.RobotHardware;
+
+public class Spindexer {
+    private CRServo servospin1, servospin2;
+    private AnalogInput position;
+    private double kp, ki, kd, ks;
+    private double targetPositon, currentPosition;
+    public enum StateSpindexer{
+        CHAMBERFRONT,
+        INTAKE,
+        CHAMBERLEFT,
+        CHAMBERRIGHT;
+    }
+
+    private StateSpindexer state, laststate;
+
+    public void init(RobotHardware robot){
+        servospin1 = robot.servoSpindexer1;
+        servospin2 = robot.servoSpindexer2;
+
+    }
+    public void update(){
+        if(state != laststate){
+            switch (state){
+                case INTAKE:
+                    targetPositon = 0;
+
+                    break;
+                case CHAMBERFRONT:
+                    targetPositon = 10;
+
+                    break;
+                case CHAMBERLEFT:
+                    targetPositon = 50;
+
+                    break;
+                case CHAMBERRIGHT:
+                    targetPositon = -20;
+
+                    break;
+            }
+
+        }
+        laststate = state;
+    }
+    currentPosition = position.getVoltage();
+}
+
